07069e147b9e2531b33aa14860f581ef901e0b04
Matthias Ettrich <matthias@relabs.io>
2020-04-07 17:02:59 +0200

    fix OPTIONS - again

--- core/backend/cors.go
+++ core/backend/cors.go
@@ -14,6 +14,7 @@ func (b *Backend) handleCORS(router *mux.Router) {
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
 				h.ServeHTTP(w, r)
 				return
 			}
