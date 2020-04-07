22352d921a8b5480b9850236580a134fec773fa9
Matthias Ettrich <matthias@relabs.io>
2020-04-07 17:21:05 +0200

    another CORS fix

--- core/backend/cors.go
+++ core/backend/cors.go
@@ -1,6 +1,7 @@
 package backend
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,12 +11,12 @@ func (b *Backend) handleCORS(router *mux.Router) {
 
 	corseMiddleware := func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Println("CORS middleware: ", r.Method)
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-			if r.Method == "OPTIONS" {
+			if r.Method == http.MethodOptions {
 				w.WriteHeader(http.StatusOK)
-				h.ServeHTTP(w, r)
 				return
 			}
 			h.ServeHTTP(w, r)
