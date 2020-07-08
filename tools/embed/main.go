7be88bfa4f84d14169358f62fd5baa9edba8db30
Gregory Lenoble <greg@dalarub.com>
2020-07-08 11:25:24 +0200

    Add JSON schema validation in the fitness service

--- tools/embed/main.go
+++ tools/embed/main.go
@@ -30,16 +30,47 @@ func main() {
 		}
 	}
 
+	allValuesString := ""
+	allValuesString = "\n// AllValues contains a list of all strings defined above\n"
+	allValuesString += fmt.Sprintf("var AllValues = []string{")
 	if len(jsonFiles) > 0 {
 		for _, j := range jsonFiles {
 			f, err := os.Open(j)
 			if err != nil {
 				panic(err)
 			}
-			out.Write([]byte(strings.TrimSuffix(f.Name(), suffix) + goSuffix + " = `"))
+			variableName := strings.TrimSuffix(f.Name(), suffix) + goSuffix
+			variableName = toUpperCamelCase(variableName)
+			allValuesString += variableName + ", "
+
+			out.Write([]byte("    // " + variableName + " ...\n"))
+			out.Write([]byte("    " + variableName + " = `"))
 			io.Copy(out, f)
 			out.Write([]byte("`\n"))
 		}
 	}
+	allValuesString = strings.TrimSuffix(allValuesString, ", ") + "}\n"
 	out.Write([]byte(")\n"))
+	out.Write([]byte(allValuesString))
+}
+
+// toUpperCamelCase convert a string to upper camel case
+func toUpperCamelCase(str string) string {
+	isToUpper := false
+	var camelCase string
+	for _, v := range str {
+		if isToUpper {
+			camelCase += strings.ToUpper(string(v))
+			isToUpper = false
+		} else {
+			if v == '_' {
+				isToUpper = true
+			} else {
+				camelCase += string(v)
+			}
+		}
+
+	}
+	return strings.Title(camelCase)
+
 }
